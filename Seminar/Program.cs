394f88470ee9c0fbd1c50a09c20a4cf03b29fbfe
sdsggrf <owlsdsggrf@gmail.com>
2023-02-26 02:44:49 +0500

    нахождение квадрата, являеться ли квадратом

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Квадрат {number} = {number * number}");
---
+++ Seminar_Work2/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 == MathF.Sqrt(number)) Console.Write("да");
+else if (number2 != MathF.Sqrt(number)) Console.Write("нет");
