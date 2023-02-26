a92f02041affba5f082a5ed8ee40d9b80802f0f5
sdsggrf <owlsdsggrf@gmail.com>
2023-02-27 04:11:08 +0500

    номер дня недели, нахождение большего числа

---
+++ Seminar1_HomeWork1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number>number2) Console.WriteLine($"число {number} больше {number2}");
+else if (number<number2) Console.WriteLine($"число {number2} больше {number}");
---
+++ Seminar_Work3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==1) Console.WriteLine("Понедельник");
+else if(number==2) Console.WriteLine("Вторник");
+else if(number==3) Console.WriteLine("Среда");
+else if(number==4) Console.WriteLine("Четверг");
+else if(number==5) Console.WriteLine("Пятница");
+else if(number==6) Console.WriteLine("Суббота");
+else if(number==7) Console.WriteLine("Воскресенье");
+else  Console.WriteLine("Введите число от 1 до 7");
