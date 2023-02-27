5fba9bcce90d46bcb44fe0f7b62040bc3ea23fe5
sdsggrf <owlsdsggrf@gmail.com>
2023-02-27 22:29:32 +0500

    задание 4, нахождение максимального числа

---
+++ Seminar1_HomeWork2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int size = 3;
+int[] arr = new int[3];
+int index;
+
+index = 0;
+int maxPos;
+maxPos = 0;
+index = 0;
+while (index < size)
+{
+    arr[index] = Convert.ToInt32(Console.ReadLine());
+   
+    
+        if (arr[maxPos] > arr[index])
+        {
+        }
+        else
+        {
+            maxPos = index;
+        }
+   
+    index = index + 1;
+}
+Console.WriteLine("Максимальное число: " + arr[maxPos]);
+
