cc206256417f45ba7ac1c74dca0124708eba184e
sdsggrf <owlsdsggrf@gmail.com>
2023-03-01 11:14:56 +0500

    новая папкак, решение задачи 11, семинар

---
+++ Seminar2_work1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int result = RemoveSecondТumber(number);
+Console.WriteLine(result);
+
+int RemoveSecondТumber(int num)
+{
+    int firstnNumber = num / 100;
+    int secondNumber = num % 100 % 10;
+    int answer = (firstnNumber * 10) + secondNumber;
+    return answer;
+}
