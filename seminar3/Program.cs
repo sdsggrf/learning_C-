def7806c6eb62b7922ff623d4a4b79365681192c
sdsggrf <owlsdsggrf@gmail.com>
2023-03-02 18:03:11 +0500

    растояние в 3d пространстве

---
+++ seminar3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+
+
+
+
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+table (number);
+
+void table(int num)
+{
+        int index = 1;
+    int result = 0;
+    while(index <= num)
+    {
+        result = index*index; 
+        Console.WriteLine($"{index,5}    {result,5}" );
+        index++;
+    }
+}
+
+
---
+++ seminar3_HomeWork1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 21 (branch task_2)
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Вариант решения с измененным кодом с семинара
+
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int xPointA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yPointA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int zPointA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int xPointB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yPointB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int zPointB = Convert.ToInt32(Console.ReadLine());
+
+
+double distance = Distance(xPointA, yPointA, xPointB, yPointB, zPointA, zPointB);
+Console.WriteLine($"Расстояние между точками равно: {distance}");
+
+double Distance(int xA, int yA, int xB, int yB, int zA, int zB)
+{
+int A = xA-xB;
+int B = yA-yB;
+int C = zA-zB;
+
+
+
+    return Math.Sqrt(A*A+B*B+C*C);
+}
+/*второй вариант решения
+Задача 21 (branch task_2)
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+
+// // Функция ввода сообщения
+// int ReadInt(string message)
+// {
+//  Console.Write(message);
+//  return Convert.ToInt32(Console.ReadLine());
+// }
