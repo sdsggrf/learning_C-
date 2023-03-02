98c8edd7db1e582a3f9c2b7f268c0dfa37de2b05
sdsggrf <owlsdsggrf@gmail.com>
2023-03-02 13:38:26 +0500

    home work 2, нахождение 3 числа

---
+++ Seminar2_HomeWork2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите трёхзначное число");
+
+int number =Convert.ToInt32(Console.ReadLine());
+if (number>100)
+{
+ int result=number/100%10;
+ Console.WriteLine(result);
+
+}
+
+else Console.WriteLine("введите число > 100 ");
