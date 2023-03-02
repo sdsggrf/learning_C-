58c58d0b996ef8128a8d6c5ea837e97d91db80ce
sdsggrf <owlsdsggrf@gmail.com>
2023-03-02 13:55:13 +0500

    home work 3, поиск выходных

---
+++ Seminar2_HomeWork3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number>7)Console.WriteLine("Не являеться днем недели");
+else if (number < 1)Console.WriteLine("Не являеться днем недели");
+else if (number>5)Console.WriteLine("наконец-то выходной");
+else Console.WriteLine("Ещё работать и работать");
