c7d2edb10850fb4691cfd54e5f913be4bfb3a443
sdsggrf <owlsdsggrf@gmail.com>
2023-03-02 18:34:51 +0500

    проверка на палиндромность

---
+++ Seminar3_HomeWork3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+Console.WriteLine($"Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2=number;
+Reverse(number);
+number = Reverse(number);
+int Reverse(int i)
+{
+    int res = 0;
+    while (i != 0)
+    {
+        int remaind;
+        i = Math.DivRem(i, 10, out remaind);
+        res = res * 10 + remaind;
+    }
+    return res;
+}
+if(number==number2)Console.WriteLine($"{number2} являеться палиндромом");
+else
+Console.WriteLine($"{number2} не являеться палиндромом");
