b395c13c39a907f98f4f8f00523cd9a58f22338c
sdsggrf <owlsdsggrf@gmail.com>
2023-03-09 01:05:15 +0500

    задача 29,добавил метод

--- Seminar4_HomeWork3/Program.cs
+++ Seminar4_HomeWork3/Program.cs
@@ -5,20 +5,24 @@
 
 
 
-int size = 8;
-int[] arr = new int[size];
-int index = 0;
+int siz = 8;
+int[] ar = new int[siz];
+int ind = 0;
+mass(siz, ar, ind);
+Write(siz, ar, ind);
 
-
-
-
+void mass(int size, int[]arr, int index){
 while (index < size)
 {
     arr[index] = new Random().Next(1, 101);
-
+    Console.Write(arr[index]);
     index = index + 1;
+    if(index<size) Console.Write(",");
 
 }
+}
+void Write(int size, int[] arr, int index){
+Console.Write(" -> ");
 index = 0;
 Console.Write("[");
 while (index < size)
@@ -26,7 +30,8 @@ while (index < size)
 
     Console.Write(arr[index]);
     index = index + 1;
-    if(index<size) Console.Write(", ");
+    if(index<size) Console.Write(",");
 
 }
 Console.Write("]");
+}
