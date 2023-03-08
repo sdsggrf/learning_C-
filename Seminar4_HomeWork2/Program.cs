0642daef6e5040df3e3cf6c26ef7747969d9a791
sdsggrf <owlsdsggrf@gmail.com>
2023-03-09 00:12:33 +0500

    задача 26

---
+++ Seminar4_HomeWork2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+
+Console.WriteLine($"Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Summa(number);
+Console.WriteLine(Summa(number));
+int Summa(int i)
+{
+    int res = 0;
+    while (i != 0)
+    {
+        int remaind;
+        i = Math.DivRem(i, 10, out remaind);
+        res = res + remaind;
+       
+    }
+    return res;
+    
+}
