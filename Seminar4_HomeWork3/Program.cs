de04c12ec20dd536836501ce6edf51880a4834b5
sdsggrf <owlsdsggrf@gmail.com>
2023-03-09 00:43:57 +0500

    задача 29

---
+++ Seminar4_HomeWork3/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+
+int size = 8;
+int[] arr = new int[size];
+int index = 0;
+
+
+
+
+while (index < size)
+{
+    arr[index] = new Random().Next(1, 101);
+
+    index = index + 1;
+
+}
+index = 0;
+Console.Write("[");
+while (index < size)
+{
+
+    Console.Write(arr[index]);
+    index = index + 1;
+    if(index<size) Console.Write(", ");
+
+}
+Console.Write("]");
