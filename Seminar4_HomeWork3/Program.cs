6443cb77bbcadbe1b860884bfa7d7dd156f8c3d9
sdsggrf <owlsdsggrf@gmail.com>
2023-03-10 22:52:41 +0500

    задача 29, разделил метод заполнения и разделения массива

--- Seminar4_HomeWork3/Program.cs
+++ Seminar4_HomeWork3/Program.cs
@@ -11,27 +11,52 @@ int ind = 0;
 mass(siz, ar, ind);
 Write(siz, ar, ind);
 
-void mass(int size, int[]arr, int index){
-while (index < size)
+void mass(int size, int[] arr, int index)
 {
-    arr[index] = new Random().Next(1, 101);
-    Console.Write(arr[index]);
-    index++;
-    if(index<size) Console.Write(",");
+    while (index < size)
+    {
+        arr[index] = new Random().Next(1, 101);
+        
+        index++;
 
+    }
 }
-}
-void Write(int size, int[] arr, int index){
-Console.Write(" -> ");
-index = 0;
-Console.Write("[");
-while (index < size) // for (int i = 0; i < count; i++) можно через это, в данном коде не работает
+void Write(int size, int[] arr, int index)
 {
+    int index1  = 0;
+    int size2=2;
+    while (index1 < size2)
+    {
+        if (index1 < 1)
+        {
+           int index2=0;
+            while (index2 < size) // for (int i = 0; i < count; i++) можно через это, в данном коде не работает
+            {
 
-    Console.Write(arr[index]);
-    index++;
-    if(index<size) Console.Write(",");
+                Console.Write(arr[index]);
+                index2++;
+                index++;
+                if (index < size) Console.Write(", ");
 
-}
-Console.Write("]");
+            }
+            index=0;
+        }
+        else{
+            Console.Write(" -> ");
+            Console.Write("[");
+            index=0;
+            int index3=0;
+            while (index3 < size) // while (index3 < size) // for (int i = 0; i < count; i++) можно через это, в данном коде не работает
+            {
+
+                Console.Write(arr[index]);
+                index3++;
+                index++;
+                if (index < size) Console.Write(", ");
+
+            }
+            Console.Write("]");
+        }
+        index1++;
+    }
 }
