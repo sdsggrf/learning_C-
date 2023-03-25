806853405fd503968a34d6663904986a644a6451
sdsggrf <owlsdsggrf@gmail.com>
2023-03-25 17:52:01 +0500

    задача 66

---
+++ Seminar9_HomeWork2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+
+
+
+
+Main();
+void Main()
+{
+    Console.Write("Введите число M: ");
+    int m = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите число N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int sum = SumNumbers(m, n); // вызов рекурсивного метода
+    Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} равна {sum}");
+}
+
+int SumNumbers(int m, int n)
+{
+    if (m > n) // базовый случай: если m больше n, возвращаем 0
+    {
+        return 0;
+    }
+    else // рекурсивный случай: суммируем m и вызываем метод с аргументами m+1 и n
+    {
+        return m + SumNumbers(m + 1, n);
+    }
+}
